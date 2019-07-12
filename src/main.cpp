13b1cc93d3b515799247cadb3c095176dabee030
Aidan Prehatny <aprehatny@yahoo.com>
2019-07-12 10:52:47 -0700

    add disconnect slots, add member connector, add example classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,28 @@
 #include "signal.h"
 #include <iostream>
 
+class Person {
+public:
+  Person(std::string const &name) : name_(name) {}
+
+  Signal<std::string const &> say;
+
+  void listen(std::string const &message) const {
+    std::cout << name_ << " received: " << message << std::endl;
+  }
+
+private:
+  std::string name_;
+};
+
 int main() {
-    // pass arguments to the function to store in the signal
-    Signal<std::string, std::string> sig;
-    sig.connect([] (std::string foo, std::string bar) {
-        return foo + bar;
-    });
-    return 0;
+  Person alice("Alice"), bob("Bob");
+
+  alice.say.connectMember(&bob, &Person::listen);
+  bob.say.connectMember(&alice, &Person::listen);
+
+  alice.say.emit("Have a nice day!");
+  bob.say.emit("Thank you!");
+
+  return 0;
 }
--- src/signal.h
+++ src/signal.h
@@ -20,10 +20,36 @@ class Signal {
             arr{ 0, (std::cout << values << '\t', 0)... };
         }
 
+        template <typename T>
+        int connectMember(T *instance, void (T::*func)(Args...) const) const {
+            return connect([=](Args... args) {
+                (instance->*func)(args...);
+            });
+        }
+
         int connect(std::function<void(Args...)> const& slot) const {
             this->slots.insert(std::make_pair(++this->currentId, slot));
             return this->currentId;
         }
+
+        void disconnect(int id) const {
+            slots.erase(id);
+        }
+
+        void disconnect_all() const {
+            slots.clear();
+        }
+
+        void emit(Args ... p) {
+            for(auto it : slots) {
+                it.second(p...);
+            }
+        }
+        
+        Signal& operator=(Signal const& other) {
+            disconnect_all();
+        }
+
 
     private:
         mutable std::map<int, std::function<void(Args...)>> slots;
